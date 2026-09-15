f3670df185e7c148c062d0fe635fcb6c5c7325c2
cereaIx <alicepopilliu@gmail.com>
2026-09-15 01:52:23 -0400

    efficient scan

--- stream_compaction/efficient.cu
+++ stream_compaction/efficient.cu
@@ -12,13 +12,70 @@ namespace StreamCompaction {
             return timer;
         }
 
+        __global__ void kernUpsweep(int n, int offset, int* data) {
+          unsigned idx = blockIdx.x * blockDim.x + threadIdx.x;
+
+          if (idx >= n) {
+            return;
+          }
+          
+          if (idx % offset == 0) {
+            data[idx + offset - 1] += data[idx + (offset / 2) - 1];
+          }
+        }
+
+        __global__ void kernDownsweep(int n, int offset, int* data) {
+          unsigned idx = blockIdx.x * blockDim.x + threadIdx.x;
+
+          if (idx >= n) {
+            return;
+          }
+
+          if (idx % offset == 0) {
+            int temp = data[idx + (offset / 2) - 1];
+            data[idx + (offset / 2) - 1] = data[idx + offset - 1];
+            data[idx + offset - 1] += temp;
+          }
+        }
+
         /**
          * Performs prefix-sum (aka scan) on idata, storing the result into odata.
          */
         void scan(int n, int *odata, const int *idata) {
-            timer().startGpuTimer();
-            // TODO
-            timer().endGpuTimer();
+          // TODO
+          int upRoundedN = 1 << ilog2ceil(n);
+
+          // device array
+          int *dev_data;
+
+          cudaMalloc((void**)&dev_data, upRoundedN * sizeof(int));
+          cudaMemset(dev_data, 0, upRoundedN * sizeof(int));
+          cudaMemcpy(dev_data, idata, n * sizeof(int), cudaMemcpyHostToDevice);
+
+          int blockSize = 128;
+          int blocksPerGrid = (upRoundedN + blockSize - 1) / blockSize;
+
+          timer().startGpuTimer();
+
+          // upsweep
+          for (int d = 0; d < ilog2ceil(n); d++) {
+            int offset = 1 << (d + 1);
+            kernUpsweep<<<blocksPerGrid, blockSize>>>(upRoundedN, offset, dev_data);
+          }
+
+          // downsweep
+          cudaMemset(&dev_data[upRoundedN - 1], 0, sizeof(int));
+          for (int d = ilog2ceil(n) - 1; d >= 0; d--) {
+            int offset = 1 << (d + 1);
+            kernDownsweep<<<blocksPerGrid, blockSize>>>(upRoundedN, offset, dev_data);
+          }
+
+          timer().endGpuTimer();
+
+          cudaMemcpy(odata, dev_data, n * sizeof(int), cudaMemcpyDeviceToHost);
+
+          // free device arrays
+          cudaFree(dev_data);
         }
 
         /**
@@ -33,6 +90,7 @@ namespace StreamCompaction {
         int compact(int n, int *odata, const int *idata) {
             timer().startGpuTimer();
             // TODO
+
             timer().endGpuTimer();
             return -1;
         }
--- stream_compaction/naive.cu
+++ stream_compaction/naive.cu
@@ -11,15 +11,58 @@ namespace StreamCompaction {
             static PerformanceTimer timer;
             return timer;
         }
-        // TODO: __global__
+        
+        __global__ void kernNaiveScan(int n, int offset, int* odata, const int* idata) {
+            unsigned idx = blockIdx.x * blockDim.x + threadIdx.x;
+
+            if (idx >= n) {
+              return;
+            }
+
+            if (idx >= offset) {
+              odata[idx] = idata[idx - offset] + idata[idx];
+            }
+            else {
+              odata[idx] = idata[idx];
+            }
+        }
 
         /**
          * Performs prefix-sum (aka scan) on idata, storing the result into odata.
          */
-        void scan(int n, int *odata, const int *idata) {
-            timer().startGpuTimer();
+        void scan(int n, int *odata, const int *idata) {  
             // TODO
+            // device arrays
+            int *dev_idata, *dev_odata;
+
+            cudaMalloc((void**)&dev_idata, n * sizeof(int));
+            cudaMalloc((void**)&dev_odata, n * sizeof(int));
+
+            cudaMemcpy(dev_idata, idata, n * sizeof(int), cudaMemcpyHostToDevice);
+
+            int blockSize = 128;
+            int blocksPerGrid = (n + blockSize - 1) / blockSize;
+
+            timer().startGpuTimer();
+            for (int d = 1; d <= ilog2ceil(n); d++) {
+              int offset = 1 << (d - 1);
+              kernNaiveScan<<<blocksPerGrid, blockSize>>>(n, offset, dev_odata, dev_idata);
+              std::swap(dev_odata, dev_idata);
+            }
             timer().endGpuTimer();
+            
+            // data is always in idata
+            cudaMemcpy(odata, dev_idata, n * sizeof(int), cudaMemcpyDeviceToHost);
+
+            // convert from inclusive to exclusive scan
+            for (int i = n - 1; i > 0; i--) {
+              odata[i] = odata[i - 1];
+            }
+            odata[0] = 0;
+
+            // free device arrays
+            cudaFree(dev_idata);
+            cudaFree(dev_odata);
         }
     }
 }
